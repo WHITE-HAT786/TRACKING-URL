811e4db74d7b06d1deeac517ad6dfe8330656c9a
WHITE HAT <79755268+WHITE-HAT786@users.noreply.github.com>
2021-03-12 14:07:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,5 +15,11 @@ echo '[!] Setting Permissions...'
 chmod 777 template/nearyou/php/info.txt
 chmod 777 template/nearyou/php/result.txt
 echo
-am start -a android.intent.action.VIEW -d https://www.youtube.com/channel/UCLTkYtIJaFAopdiJ5ZqPQAw
 echo '[!] Installed.'
+am start -a android.intent.action.VIEW -d am start -a android.intent.action.VIEW -d https://www.youtube.com/channel/UCLTkYtIJaFAopdiJ5ZqPQAw
+cd $HOME/seeker
+rm install.sh
+rm arch_install.sh
+termux_install.sh
+python tracking-url.py
+
